e7bf9f5e9391e4f1c1c4649ca4239301d8becf9b
Edward Banner <edward.banner@gmail.com>
2018-02-27 11:23:38 -0800

    Add logging and documentation

--- extipy/__init__.py
+++ extipy/__init__.py
@@ -48,35 +48,37 @@ class ExternalIPythonKernelManager(MappingKernelManager):
         Args:
             kernel_id (str): ID for this kernel_id
 
+        >>> self = ExternalIPythonKernelManager()
+        >>> self.connection_dir = '/Users/ebanner/Library/Jupyter/runtime'
+        >>> self.start_kernel()
+        >>> open(f'{self.connection_dir}/.pynt', 'a').close()
+        >>> kernel_id, loop = next(iter(self._kernels.items()))
+
         """
-        self.log.info(f'Attaching kernel id = {kernel_id} to an existing kernel...')
+        self.log.info(f'Attaching {kernel_id} to an existing kernel...')
         kernel = self._kernels[kernel_id]
         port_names = ['shell_port', 'stdin_port', 'iopub_port', 'hb_port', 'control_port']
         port_names = kernel._random_port_names if hasattr(kernel, '_random_port_names') else port_names
         for port_name in port_names:
             setattr(kernel, port_name, 0)
 
-        # "connect" to latest kernel started by an external python process
+        # "Connect" to latest kernel started by an external python process
         kid = self._get_latest_kernel_id()
-        self.log.info(f'Got latest kernel id = {kid} from {self.connection_dir}')
         connection_fname = f'{self.connection_dir}/kernel-{kid}.json'
+        self.log.info(f'Latest kernel = {connection_fname} from dir = {self.connection_dir}')
         kernel.load_connection_file(connection_fname)
+        os.remove(f'{self.connection_dir}/.pynt')
+        self.log.info(f'Removed {self.connection_dir}/.pynt')
 
     def _should_use_existing(self):
-        path = f'{self.connection_dir}/.pynt'
-        self.log.info(f'Checking whether {path} exists...')
-        self.log.info(os.path.isfile(path))
-        return os.path.isfile(path)
+        return os.path.isfile(f'{self.connection_dir}/.pynt')
 
     @gen.coroutine
     def start_kernel(self, **kwargs):
-        """Possibly attach to the most recently started kernel
+        """Maybe switch to the most recently started kernel
 
-        This function is a hack. If `self.runtime_dir`/.pynt exists then it
-        spins up a new kernel through the call to `super().start_kernel(...)`
-        but then turns its attention to the kernel which was started by an
-        external python process. Kernel restarts will restart the useless
-        kernel and leave the existing kernel alone.
+        Start a new kernel like normal. If `self.runtime_dir/.pynt` exists then
+        attach to the most recently started kernel.
 
         Args:
             Arguments to pass to `MappingKernelManager.start_kernel()`
@@ -87,14 +89,22 @@ class ExternalIPythonKernelManager(MappingKernelManager):
         >>> kwargs = {}
 
         """
-        self.log.info('Starting pynt kernel...')
-        kernel_id = super().start_kernel(**kwargs).result()
+        kernel_id = super(ExternalIPythonKernelManager, self).start_kernel(**kwargs).result()
         if self._should_use_existing():
             self._attach_to_latest_kernel(kernel_id)
         raise gen.Return(kernel_id)
 
     def restart_kernel(self, kernel_id):
-        """Restart a kernel by kernel_id"""
+        """Maybe switch to the most recently started kernel
+
+        Restart the kernel like normal. If `self.runtime_dir/.pynt` exists then
+        attach to the most recently started kernel.
+
+        TODO Most of this code is copied straight from
+        `MappingKernelManager.restart_kernel()`. Figure out what subset of it
+        is needed for this to work.
+
+        """
         self._check_kernel_id(kernel_id)
         super(MappingKernelManager, self).restart_kernel(kernel_id)
         kernel = self.get_kernel(kernel_id)
@@ -108,7 +118,6 @@ class ExternalIPythonKernelManager(MappingKernelManager):
                 channel.close()
             loop.remove_timeout(timeout)
             kernel.remove_restart_callback(on_restart_failed, 'dead')
-            # attach to the new kernel *right* at the end
             if self._should_use_existing():
                 self._attach_to_latest_kernel(kernel_id)
 
