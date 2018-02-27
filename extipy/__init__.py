f8fbc48afe2c451bbb51c693aa1af67616d1bd89
Edward Banner <edward.banner@gmail.com>
2018-02-26 17:20:07 -0800

    Add code for switching during a kernel restart

--- extipy/__init__.py
+++ extipy/__init__.py
@@ -9,6 +9,7 @@ kernel started outside of Jupyter.
 
 import glob
 import os
+import os.path
 import re
 
 from tornado import gen
@@ -31,6 +32,8 @@ class ExternalIPythonKernelManager(MappingKernelManager):
         Returns:
             kid (int): the ID of the kernel file which was modified most recently
 
+        >>> self = ExternalIPythonKernelManager()
+
         """
         conn_fnames = glob.glob(f'{self.connection_dir}/kernel-*.json')
         p = '.*kernel-(?P<kid>\d+).json'
@@ -39,23 +42,14 @@ class ExternalIPythonKernelManager(MappingKernelManager):
         kid = re.match(p, latest_conn_fname).group('kid')
         return kid
 
-    @gen.coroutine
-    def start_kernel(self, **kwargs):
-        """Attach to the most recently started kernel
-
-        This function is a hack. It spins up a new kernel through the call to
-        `super().start_kernel(...)` but then turns its attention to the kernel
-        which was started by an external python process. Kernel restarts will
-        restart the useless kernel and leave the existing kernel alone.
+    def _attach_to_latest_kernel(self, kernel_id):
+        """Attch to the latest externally started IPython kernel
 
         Args:
-            Arguments to pass to `MappingKernelManager.start_kernel()`
+            kernel_id (str): ID for this kernel_id
 
         """
-        # start a new kernel with ipykernel
-        kernel_id = super().start_kernel(**kwargs).result()
-
-        # "zero-out" the ports connected to that kernel
+        self.log.info(f'Attaching kernel id = {kernel_id} to an existing kernel...')
         kernel = self._kernels[kernel_id]
         port_names = ['shell_port', 'stdin_port', 'iopub_port', 'hb_port', 'control_port']
         port_names = kernel._random_port_names if hasattr(kernel, '_random_port_names') else port_names
@@ -64,8 +58,52 @@ class ExternalIPythonKernelManager(MappingKernelManager):
 
         # "connect" to latest kernel started by an external python process
         kid = self._get_latest_kernel_id()
+        self.log.info(f'Got latest kernel id = {kid} from {self.connection_dir}')
         connection_fname = f'{self.connection_dir}/kernel-{kid}.json'
         kernel.load_connection_file(connection_fname)
 
-        # py2-compat
+    def _should_use_existing(self):
+        return os.path.isfile(f'{self.connection_dir}/.pynt')
+
+    @gen.coroutine
+    def start_kernel(self, **kwargs):
+        """Possibly attach to the most recently started kernel
+
+        This function is a hack. If `self.runtime_dir`/.pynt exists then it
+        spins up a new kernel through the call to `super().start_kernel(...)`
+        but then turns its attention to the kernel which was started by an
+        external python process. Kernel restarts will restart the useless
+        kernel and leave the existing kernel alone.
+
+        Args:
+            Arguments to pass to `MappingKernelManager.start_kernel()`
+
+        >>> self = ExternalIPythonKernelManager()
+        >>> self.connection_dir = '/Users/ebanner/Library/Jupyter/runtime'
+        >>> __class__ = ExternalIPythonKernelManager
+        >>> kwargs = {}
+
+        """
+        self.log.info('Starting kernel!')
+        kernel_id = super().start_kernel(**kwargs).result()
+        if self._should_use_existing():
+            self._attach_to_latest_kernel(kernel_id)
         raise gen.Return(kernel_id)
+
+    # def restart_kernel(self, kernel_id):
+    #     """Possibly attach to the most recently started kernel
+
+    #     Args:
+    #         Arguments to pass to `MappingKernelManager.start_kernel()`
+
+    #     >>> self = ExternalIPythonKernelManager()
+    #     >>> self.connection_dir = '/Users/ebanner/Library/Jupyter/runtime'
+    #     >>> kernel_id = self.start_kernel().result()
+    #     >>> import time; time.sleep(1)
+    #     >>> __class__ = ExternalIPythonKernelManager
+
+    #     """
+    #     future = MappingKernelManager.restart_kernel(self, kernel_id)
+    #     if self._should_use_existing():
+    #         self._attach_to_latest_kernel(kernel_id)
+    #     return future
