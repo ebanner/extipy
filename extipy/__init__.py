2b36a6bb5f5d6eef920af136e0d14df2918c43d6
Edward Banner <edward.banner@gmail.com>
2025-03-29 09:40:36 -0400

    Get latest started kernel

--- extipy/__init__.py
+++ extipy/__init__.py
@@ -8,8 +8,27 @@ from jupyter_client import KernelProvisionerBase
 import logging
 import json
 import os
+import subprocess
+import glob
 
 _log = logging.getLogger(__name__)
+
+
+def get_latest_connection_file():
+    def get_jupyter_runtime_dir():
+        result = subprocess.run(
+            ['jupyter', '--runtime-dir'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            check=True
+        )
+        return result.stdout.strip()
+
+    jupyter_runtime_dir = get_jupyter_runtime_dir()
+    connection_filenames = glob.glob(f'{jupyter_runtime_dir}/kernel-*.json')
+    latest_connection_filename = max(connection_filenames, key=os.path.getctime)
+    return latest_connection_filename
 
 
 class ExistingProvisioner(KernelProvisionerBase):
@@ -18,10 +37,9 @@ class ExistingProvisioner(KernelProvisionerBase):
     The kernel connection file is set in the environment variable
     'PYXLL_IPYTHON_CONNECTION_FILE'.
     """
-
     async def launch_kernel(self, cmd, **kwargs):
         # Connect to kernel started by PyXLL
-        connection_file = os.environ["PYXLL_IPYTHON_CONNECTION_FILE"]
+        connection_file = get_latest_connection_file()
         if not os.path.abspath(connection_file):
             connection_dir = os.path.join(os.environ["APPDATA"], "jupyter", "runtime")
             connection_file = os.path.join(connection_dir, connection_file)
